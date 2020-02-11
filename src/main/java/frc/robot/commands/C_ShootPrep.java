754e89b1d73ef3f4cd980b82f295b0c630b6f78c
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2020-02-10 17:38:02 -0800

    fix C_ShootPrep stopping conditions

--- src/main/java/frc/robot/commands/C_ShootPrep.java
+++ src/main/java/frc/robot/commands/C_ShootPrep.java
@@ -16,33 +16,30 @@ public class C_ShootPrep extends CommandBase {
   
   private SS_Shooter shooter;
   private SS_Feeder feeder;
+  private double initialDistance = 0; //initial distance of the feeder belt\
 
   public C_ShootPrep(SS_Shooter shooter, SS_Feeder feeder) {
     this.shooter = shooter;
     this.feeder = feeder;
+    initialDistance = feeder.getBeltDistance();
     addRequirements(shooter, feeder);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     shooter.setSpinning(true);
     feeder.setRPM(FeedRate.LOAD);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     feeder.setRPM(FeedRate.IDLE);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return feeder.exitIsValidTarget();
+    //check if there is a ball at the end of the feeder ready to go into the shooter or if the belt has already travelled far
+    //enough to move a ball al the way across the belt (meaning no balls are on the belt)
+    return feeder.exitIsValidTarget() || feeder.getBeltDistance() >= feeder.REV_PER_FULL_FEED - initialDistance;
   }
 }
