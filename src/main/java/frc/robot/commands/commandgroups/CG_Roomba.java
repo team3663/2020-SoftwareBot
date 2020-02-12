fd19ad2179e62546181dda94fa71c1b42d7bd8ec
Reaganosh <reaganosh@gmail.com>
2020-02-11 19:31:38 -0800

    Fixed C_Roomba
    Fixed the code for C_Roomba

--- src/main/java/frc/robot/commands/commandgroups/CG_Roomba.java
+++ src/main/java/frc/robot/commands/commandgroups/CG_Roomba.java
@@ -1,15 +1,18 @@
-/*package frc.robot.commands.commandgroups;
+package frc.robot.commands.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.C_SetArmPosition;
 import frc.robot.commands.C_SetPickUpMotorSpeed;
+import frc.robot.subsystems.SS_Intake;
 import frc.robot.util.IntakePosition;
 
 public class CG_Roomba extends ParallelCommandGroup {
+    private final SS_Intake ss_Intake = new SS_Intake();
+
     public CG_Roomba() {
         addCommands(
-            new C_SetArmPosition(IntakePosition.FULLY_EXTENDED),
-            new C_SetPickUpMotorSpeed()
+            new C_SetArmPosition(ss_Intake, IntakePosition.FULLY_EXTENDED),
+            new C_SetPickUpMotorSpeed(ss_Intake)
         );
     }
-}*/
+}
