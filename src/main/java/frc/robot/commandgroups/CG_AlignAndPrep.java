8b53398c3dc89d51d06823a65915ba2dfe044267
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2020-02-14 17:37:47 -0800

    delete broken import in CG_AlignAndPrep

--- src/main/java/frc/robot/commandgroups/CG_AlignAndPrep.java
+++ src/main/java/frc/robot/commandgroups/CG_AlignAndPrep.java
@@ -8,7 +8,6 @@
 package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.commands.C_PrepBeltToShoot;
 import frc.robot.commands.C_PrepShootingBelt;
 import frc.robot.commands.C_Track;
 import frc.robot.drivers.Vision;
